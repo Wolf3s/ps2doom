d3a291708a8aa5dae77485a492239d188e52c9d7
Wolfywolfy <andregui17@outlook.com>
2022-02-01 14:28:57 -0300

    remove my message

--- i_sound.c
+++ i_sound.c
@@ -24,7 +24,6 @@
 // static const char rcsid[] = "$Id: i_unix.c,v 1.5 1997/02/03 22:45:10 b1 Exp $";
 
 #include <math.h>
-//Isto daqui provavelmente é do sdl não vou mexer aqui.
 #include <SDL/SDL_audio.h>
 #include <SDL/SDL_mutex.h>
 #include <SDL/SDL_byteorder.h>
