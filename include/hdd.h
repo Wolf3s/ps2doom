c93370c2010f9d260961c4c780d3289dbaa2bbdc
Wolfywolfy <andregui17@outlook.com>
2022-03-07 17:47:38 -0300

    improve hdd

--- include/hdd.h
+++ include/hdd.h
@@ -4,10 +4,11 @@
 //taken from open ps2 loader but adopted for ps2doom 
 #include <libhdd.h>
 #include <tamtypes.h>
+#include "d_main.h"
 
-//hdd and pfs whole structure
+//pfs whole structure
 #define PFS_INODE_MAX_BLOCKS 114
-#define HDD_MODE_UPDATE_DELAY MENU_UPD_DELAY_NOUPDATE
+#define HDD_MODE_UPDATE_DELAY 
 
 // APA Partition
 #define APA_IOCTL2_GETHEADER 0x6836
@@ -94,11 +95,11 @@ typedef struct
 } pfs_inode_t;
 //end
 
-/*
+
 typedef struct
 {
     char partition_name[APA_IDMAX + 1];
-    char name[HDL_GAME_NAME_MAX + 1];
+    char name[MAXWADFILES];
     char startup[8 + 1 + 3 + 1];
     u8 hdl_compat_flags;
     u8 ops2l_compat_flags;
@@ -108,13 +109,13 @@ typedef struct
     u32 layer_break;
     u32 start_sector;
     u32 total_size_in_kb;
-} hdl_game_info_t;
+} hdl_wad_info_t;
 
 typedef struct
 {
     u32 count;
-    hdl_game_info_t *games;
-} hdl_games_list_t;
+    hdl_wad_info_t *wads;
+} hdl_wad_list_t;
 
 typedef struct
 {
@@ -122,24 +123,6 @@ typedef struct
     u32 length;
 } apa_subs;
 
-int hddReadSectors(u32 lba, u32 nsectors, void *buf);
 
-// Array should be APA_MAXSUB+1 entries.
-int hddGetPartitionInfo(const char *name, apa_sub_t *parts);
-
-// Array should be max entries.
-int hddGetFileBlockInfo(const char *name, const apa_sub_t *subs, pfs_blockinfo_t *blocks, int max);
-
-int hddCheck(void);
-u32 hddGetTotalSectors(void);
-int hddIs48bit(void);
-int hddSetTransferMode(int type, int mode);
-void hddSetIdleTimeout(int timeout);
-void hddSetIdleImmediate(void);
-
-void hddInit();
-
-void hddLoadModules(void);
-*/
 
 #endif
